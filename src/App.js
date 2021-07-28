af8bfe03b2d68780c628c29aa9d7166ef80489aa
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-07-29 00:02:12 +0530

    template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './Components/NavBar'
+import HeroSection from './Components/HeroSection'
+import FeatureSection from './Components/FeatureSection'
+import CardSection from './Components/CardSection'
+import Footer from './Components/Footer'
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <HeroSection />
+      <FeatureSection />
+      <CardSection />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
